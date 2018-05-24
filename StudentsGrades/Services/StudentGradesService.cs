0c5b69f9ade11cc143d8f72350fc9c910c086f47
Krzysztof Szymczyk <krzysztof.szymczyk@yahoo.com>
2018-05-24 18:33:37 +0200

    StudentGradesService added

---
+++ StudentsGrades/Services/StudentGradesService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using StudentsGrades.Models;
+
+namespace StudentsGrades.Services
+{
+    public class StudentGradesService
+    {
+        public decimal Calculate(IEnumerable<Grade> grades)
+        {
+            var gradesSum = grades.Sum(g => g.Value * g.Weight);
+            return gradesSum / grades.Sum(x => x.Weight);
+        }
+    }
+}
