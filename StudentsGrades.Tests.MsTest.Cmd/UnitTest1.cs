0a9224eb1d8fcc94c131812ea2a0046089682cf5
Krzysztof Szymczyk <krzysztof.szymczyk@yahoo.com>
2018-07-27 15:33:44 +0200

    Test unit project added from console

---
+++ StudentsGrades.Tests.MsTest.Cmd/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StudentsGrades.Tests.MsTest.Cmd
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+			Assert.IsTrue(true);
+        }
+    }
+}
