337fbd2f4a5479341a8df22f16d33cbd89e382c8
Krzysztof Szymczyk <krzysztof.szymczyk@yahoo.com>
2018-07-30 14:30:18 +0200

    Added xUnit theory and inlinedata tests example

---
+++ StudentsGrades.Tests.XUnit/PresentationPurposesOnly/TheoryInlineDataTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace StudentsGrades.Tests.XUnit.PresentationPurposesOnly
+{
+    public class TheoryInlineDataTests
+    {
+        [Theory]
+        [InlineData(1,2)]
+        [InlineData(3,4)]
+        [InlineData(5,6)]
+        public void InlineDataTest(int a,int b)
+        {
+            Assert.NotEqual(a,b);
+        }
+    }
+}
