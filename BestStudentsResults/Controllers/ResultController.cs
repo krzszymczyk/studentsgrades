a968495a5ceb158046b0f04b4010f40a3d9344ff
Krzysztof Szymczyk <krzysztof.szymczyk@yahoo.com>
2018-08-20 15:35:50 +0200

    Post method implemented

---
+++ BestStudentsResults/Controllers/ResultController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BestStudentsResults.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BestStudentsResults.Controllers
+{
+    [Route("api/result")]
+    public class ResultController : Controller
+    {
+        public IActionResult Post([FromBody]StudentResultViewModel model)
+        {
+            return Json(model);
+        }
+    }
+}
---
+++ BestStudentsResults/ViewModels/StudentResultViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BestStudentsResults.ViewModels
+{
+    public class StudentResultViewModel
+    {
+        public Guid StudentId { get; set; }
+        public int Rating { get; set; }
+    }
+}
