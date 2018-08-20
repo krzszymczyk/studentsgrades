d61662d2e9ecaf4ed884c72fe1a88df2c19e2dff
Krzysztof Szymczyk <krzysztof.szymczyk@yahoo.com>
2018-08-20 16:37:42 +0200

    Added migration and EF Core

---
+++ BestStudentsResults/Data/Context/StudentResultsDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BestStudentsResults.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BestStudentsResults.Data.Context
+{
+    public class StudentResultsDbContext:DbContext
+        
+    {
+        public StudentResultsDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+        public DbSet<StudentResult> StudentsResults { get; set; }
+    }
+}
---
+++ BestStudentsResults/Migrations/20180820143510_InitialMigration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BestStudentsResults.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StudentsResults",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    StudentId = table.Column<Guid>(nullable: false),
+                    Rating = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentsResults", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentsResults");
+        }
+    }
+}
